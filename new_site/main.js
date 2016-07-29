708cedebecb90614b8cb7a87502138c4f0ebd76c
Randall Fine <randallfine@gmail.com>
2016-07-29 01:18:04 -0500

    updates

--- new_site/main.js
+++ new_site/main.js
@@ -1,3 +1,7 @@
-$(document).ready(function(){
-    $(".welcome").hide(0).delay(500).fadeIn(3000).delay(200).fadeOut(3000);
-});
+/*$(document).ready(function(){
+    $(".welcome").hide(0).delay(500).fadeIn(3000).delay(200).fadeOut(3000,function(){
+      $(".show").hide(0).delay(500).fadeIn(3000).delay(200).fadeOut(3000,function(){
+        $(".start").hide(0).delay(500).fadeIn(3000);
+        });
+    });
+});*/
