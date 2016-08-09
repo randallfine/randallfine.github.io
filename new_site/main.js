96ba3e8d2c54b92ee5abee12c204249cdb5bb3e9
randallfine <randallfine@gmail.com>
2016-08-09 03:04:23 -0500

    updates

--- new_site/main.js
+++ new_site/main.js
@@ -1,7 +1,12 @@
-/*$(document).ready(function(){
-    $(".welcome").hide(0).delay(500).fadeIn(3000).delay(200).fadeOut(3000,function(){
-      $(".show").hide(0).delay(500).fadeIn(3000).delay(200).fadeOut(3000,function(){
-        $(".start").hide(0).delay(500).fadeIn(3000);
+$(document).ready(function(){
+    $(".welcome").hide(0).delay(500).fadeIn(3000,function(){
+      $(".my").hide(0).delay(500).fadeIn(3000,function(){
+        $(".here").hide(0).delay(500).fadeIn(3000);
         });
     });
-});*/
+});
+$(".arrow").click(function(){
+    $(".info")
+  .transition('slide up')
+;
+})
