05de6706e6fb9a27e4f6e7241a22ee29f0c2305e
randallfine <randallfine@gmail.com>
2016-09-14 18:08:04 -0500

    working on site v2

---
+++ new_site/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $(".welcome").hide(0).delay(500).fadeIn(3000, function() {
+        $(".my").hide(0).delay(500).fadeIn(3000, function() {
+            $(".here").hide(0).delay(500).fadeIn(3000);
+            $(".buttons").hide(0).delay(500).fadeIn(3000);
+              $(".rotate").hide(0).delay(500).fadeIn(3000);
+        });
+    });
+});
+
+$(".arrow").click(function(){
+  $('html, body').animate({
+      scrollTop: $(".projects").offset().top
+  }, 2000);
+})
