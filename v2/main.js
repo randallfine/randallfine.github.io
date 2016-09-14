1889ef482b4af3f7a2af9441d7564d89f1e8c97d
randallfine <randallfine@gmail.com>
2016-09-14 18:06:45 -0500

    working on site v2

--- v2/main.js
+++ v2/main.js
@@ -3,11 +3,13 @@ $(document).ready(function() {
         $(".my").hide(0).delay(500).fadeIn(3000, function() {
             $(".here").hide(0).delay(500).fadeIn(3000);
             $(".buttons").hide(0).delay(500).fadeIn(3000);
+              $(".rotate").hide(0).delay(500).fadeIn(3000);
         });
     });
 });
+
 $(".arrow").click(function(){
   $('html, body').animate({
-      scrollTop: $("#b").offset().top
+      scrollTop: $(".projects").offset().top
   }, 1000);
 })
