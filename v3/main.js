cb15a51a970b67fe9f0748f4060bed810efc9e53
randallfine <randallfine@gmail.com>
2016-10-20 17:48:15 -0500

    commiting v3

---
+++ v3/main.js
@@ -0,0 +1,16 @@
+// $(document).ready(function() {
+//     $(".welcome").hide(0).delay(500).fadeIn(3000, function() {
+//         $(".my").hide(0).delay(500).fadeIn(3000, function() {
+//             $(".here").hide(0).delay(500).fadeIn(3000);
+//             $(".buttons").hide(0).delay(500).fadeIn(3000);
+//               $(".rotate").hide(0).delay(500).fadeIn(3000);
+//         });
+//     });
+// });
+
+
+$(".down").click(function(){
+  $('html, body').animate({
+      scrollTop: $(".projects").offset().top
+  }, 1000);
+})
